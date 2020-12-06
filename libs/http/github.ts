41f821a5a6966e379209d5e412626caa9888fbea
Milan Felix Šulc <rkfelix@gmail.com>
2020-12-06 17:18:26 +0100

    Lib: added github api methods

--- libs/http/github.ts
+++ libs/http/github.ts
@@ -1,19 +1,39 @@
 import * as httpclient from "./httpclient";
 
 export async function fetchOrganization(org: string): Promise<GithubOrg> {
-  const data = await httpclient.get(`/orgs/${org}`);
+  const res = await httpclient.get(`/orgs/${org}`);
 
-  return data as GithubOrg;
+  return res as GithubOrg;
 }
 
 export async function fetchRepositories(org: string): Promise<GithubOrg> {
-  const data = await httpclient.get(`/orgs/${org}/repos?per_page=200&type=public`);
+  const res = await httpclient.get(`/orgs/${org}/repos?per_page=200&type=public`);
 
-  return data as GithubOrg;
+  return res as GithubOrg;
 }
 
 export async function fetchTeam(org: string): Promise<GithubMember[]> {
-  const data = await httpclient.get(`/orgs/${org}/members`);
+  const res = await httpclient.get(`/orgs/${org}/members`);
 
-  return data as GithubMember[];
+  return res as GithubMember[];
+}
+
+export async function fetchRepoWebhooks(org: string, repo: string): Promise<GithubRepo> {
+  const res = await httpclient.get(`/repos/${org}/${repo}/hooks`);
+
+  return res as GithubRepo;
+}
+
+export async function deleteRepoGitterWebhook(org: string, repo: string, webhook: any): Promise<void> {
+  // Skip non-gitter webhooks
+  if (!/https:\/\/webhooks.gitter.im\/e\/[a-zA-Z0-9]+/.test(webhook.config.url)) return;
+
+  const res = await httpclient.delete(`/repos/${org}/${repo}/hooks/${webhook.id}`);
+  console.log(`[${org}/${repo}]: removed webhook: ${webhook.config.url}`);
+}
+
+export async function patchRepository(org: string, repo: string, data: any) {
+  const res = await httpclient.patch(`/repos/${org}/${repo}`, data);
+
+  return res as GithubRepo;
 }
