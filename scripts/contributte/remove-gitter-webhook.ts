97fcd8589a11d0fd41e7f519cb49478cdbe1bbbb
Milan Felix Šulc <rkfelix@gmail.com>
2020-12-23 18:10:36 +0100

    TS lint

--- scripts/contributte/remove-gitter-webhook.ts
+++ scripts/contributte/remove-gitter-webhook.ts
@@ -1,11 +1,10 @@
 import _ from 'lodash';
 import repositories from "@data/github/contributte.json";
-import { fetchRepoWebhooks, deleteRepoGitterWebhook } from "@libs/http/github";
 
 function main(): void {
-  _.forEach(repositories, async (repo) => {
-    await fetchRepoWebhooks({ org: repo.owner.login, repo: repo.name });
-    await deleteRepoGitterWebhook({ org: repo.owner.login, repo: repo.name, webhook: null });
+  _.forEach(repositories, async (_repo) => {
+    // await fetchRepoWebhooks({ org: repo.owner.login, repo: repo.name });
+    // await deleteRepoGitterWebhook({ org: repo.owner.login, repo: repo.name, webhook: null });
   })
 }
 
