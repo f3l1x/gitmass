d163942dc611d7e5ac23380b6df83c84d3ccf2b3
Milan Felix Šulc <rkfelix@gmail.com>
2020-12-23 17:55:59 +0100

    Scripts: reworked team

--- scripts/contributte/generate-team.ts
+++ scripts/contributte/generate-team.ts
@@ -1,17 +1,22 @@
 import _ from "lodash";
 import fs from "fs";
-import { fetchTeam } from "@libs/http/github";
+import { octokit } from "@libs/http/github";
+import path from "path";
+import { DATA_DIR } from "@app/config";
 
 async function dump() {
-  const githubTeam = await fetchTeam({ org: 'contributte' });
-  const team = githubTeam.map(member => {
+  const resMembers = await octokit.orgs.listMembers({
+    org: 'contributte',
+  });
+
+  const team = resMembers.data.map(member => {
     return {
-      username: member.login,
-      avatar: member.avatar_url,
+      username: member?.login,
+      avatar: member?.avatar_url,
     }
   });
 
-  fs.writeFileSync(__dirname + `/../../data/contributte/team.json`, JSON.stringify(team, null, 2));
+  fs.writeFileSync(path.join(DATA_DIR, 'contributte/team.json'), JSON.stringify(team, null, 2));
 }
 
 (async () => {
