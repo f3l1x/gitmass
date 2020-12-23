4df75ee47229642a7d14330fa7de53b61e228053
Milan Felix Šulc <rkfelix@gmail.com>
2020-12-23 18:15:07 +0100

    Scripts: simplify gitter webhook

--- scripts/contributte/remove-gitter-webhook.ts
+++ scripts/contributte/remove-gitter-webhook.ts
@@ -1,12 +1,33 @@
 import _ from 'lodash';
 import repositories from "@data/github/contributte.json";
+import { octokit } from "@libs/http/github";
+
+async function fetchRepoWebhooks(ctx: { org: string, repo: string }): Promise<GithubRepo> {
+  const res = await octokit.request(`GET /repos/${ctx.org}/${ctx.repo}/hooks`);
+
+  return res as GithubRepo;
+}
+
+async function deleteRepoGitterWebhook(ctx: { org: string, repo: string, webhook: any }): Promise<any> {
+  // Skip non-gitter webhooks
+  if (!/https:\/\/webhooks.gitter.im\/e\/[a-zA-Z0-9]+/.test(ctx.webhook.config.url)) return;
+
+  const res = await octokit.request(`DELETE /repos/${ctx.org}/${ctx.repo}/hooks/${ctx.webhook.id}`);
+  console.log(`[${ctx.org}/${ctx.repo}]: removed webhook: ${ctx.webhook.config.url}`);
+
+  return res.data;
+}
 
 function main(): void {
-  _.forEach(repositories, async (_repo) => {
-    // await fetchRepoWebhooks({ org: repo.owner.login, repo: repo.name });
-    // await deleteRepoGitterWebhook({ org: repo.owner.login, repo: repo.name, webhook: null });
+  _.forEach(repositories, async (repo) => {
+    await fetchRepoWebhooks({ org: repo.owner.login, repo: repo.name });
+    await deleteRepoGitterWebhook({ org: repo.owner.login, repo: repo.name, webhook: null });
   })
 }
 
 // @wanted
-(async () => main())();
+(async () => {
+  if (false) {
+    main();
+  }
+})();
