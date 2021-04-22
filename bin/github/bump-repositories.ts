3d62c27092dd5b3be5416986bb7f95e41ff658b7
Milan Felix Šulc <rkfelix@gmail.com>
2021-04-22 22:33:11 +0200

    Bin: download repositories with pagination (contributte has too much repositories)

--- bin/github/bump-repositories.ts
+++ bin/github/bump-repositories.ts
@@ -8,8 +8,12 @@ import organizations from "@data/github/organizations.json";
 async function dump() {
   _.forEach(Object.values(organizations) as Data.Organization[], async (org) => {
     try {
-      const resRepos = await octokit.request(`GET /orgs/${org.webalize}/repos?per_page=200&type=public`);
-      fs.writeFileSync(path.join(DATA_DIR, `github/${org.webalize}.json`), JSON.stringify(resRepos.data, null, 2));
+      const repos = [];
+      for (const page of [1, 2]) {
+        const resRepos = await octokit.request(`GET /orgs/${org.webalize}/repos?per_page=100&page=${page}&type=public`);
+        repos.push(...resRepos.data);
+      }
+      fs.writeFileSync(path.join(DATA_DIR, `github/${org.webalize}.json`), JSON.stringify(repos, null, 2));
     } catch (e) {
       console.log(e);
     }
