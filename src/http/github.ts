e21819bccea08750120a60d7ae21c35486a605d3
Milan Felix Šulc <rkfelix@gmail.com>
2021-05-20 23:22:31 +0200

    Github: fail if no token is provided

--- src/http/github.ts
+++ src/http/github.ts
@@ -1,3 +1,8 @@
 import { Octokit } from "@octokit/rest";
 
+if (!process.env.GITHUB_TOKEN_GITMASS) {
+  console.error('Missing GITHUB_TOKEN_GITMASS');
+  process.exit(255);
+}
+
 export const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN_GITMASS, userAgent: 'Gitmass v0.1' });
